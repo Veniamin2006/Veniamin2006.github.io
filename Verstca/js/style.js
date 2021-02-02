42762ce6931b75d40eb8ada9eb34c7ec6f8de131
Veniamin2006 <78445996+Veniamin2006@users.noreply.github.com>
2021-02-02 22:41:45 +0200

    Delete Verstca directory

--- Verstca/js/style.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-
-	$('.header__menu-btn').on('click', function () {
-		 $('.header__menu-list').slideToggle();
-	});
-	$('.questions__accordion-item__tridder').click(function() {
-		$(this).next('.questions__accordion-item__content').slideToggle();
-	});
-});
