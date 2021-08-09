e21b4675d8a735186d56c9676c823a2fae5e5c5f
Veniamin2006 <78445996+Veniamin2006@users.noreply.github.com>
2021-08-09 15:03:58 +0300

    Update main.js

--- site-8/js/main.js
+++ site-8/js/main.js
@@ -13,4 +13,42 @@ for (let i = 0; i < accordionButtons.length; i++) {
 			accordionContent.style.maxHeight = accordionContent.scrollHeight + "px";
 		}
 	});
+};
+
+
+const isMobile = {
+	Andriod: function () {
+		return navigator.userAgent.match(/Andriod/i);
+	},
+	BlackBerry: function () {
+		return navigator.userAgent.match(/BlackBerry/i);
+	},
+	iOS: function () {
+		return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+	},
+	Opera: function () {
+		return navigator.userAgent.match(/Opera mini/i);
+	},
+	Windows: function () {
+		return navigator.userAgent.match(/IEMobile/i);
+	},
+	any: function () {
+		return (
+			isMobile.Andriod() ||
+			isMobile.BlackBerry() ||
+			isMobile.iOS() ||
+			isMobile.Opera() ||
+			isMobile.Windows());
+	}
+};
+
+// Меню Бургер
+const iconMenu = document.querySelector('.header-burger-icon');
+if (iconMenu) {
+	const menuBody = document.querySelector('.header-burger-body');
+	iconMenu.addEventListener("click", function (e) {
+		document.body.classList.toggle('lock');
+		iconMenu.classList.toggle('active');
+		menuBody.classList.toggle('active');
+	});
 }
