33e65c79ca3ca9c3832d8084955dd4d89b031c04
Veniamin2006 <78445996+Veniamin2006@users.noreply.github.com>
2021-08-09 08:51:00 +0300

    Add files via upload

---
+++ site-8/js/main.js
@@ -0,0 +1,16 @@
+let accordionButtons = document.getElementsByClassName('faq-item-button');
+
+
+for (let i = 0; i < accordionButtons.length; i++) {
+	accordionButtons[i].addEventListener('click', function () {
+		this.classList.toggle('active');
+		let accordionContent = this.nextElementSibling;
+
+		if (accordionContent.style.maxHeight) {
+			accordionContent.style.maxHeight = null;
+		}
+		else {
+			accordionContent.style.maxHeight = accordionContent.scrollHeight + "px";
+		}
+	});
+}
